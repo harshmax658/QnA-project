f0313d70ad63dd788da01b6b04f7a401f17c6474
harshmax658 <harshmax658@gmail.com>
2022-04-22 14:42:26 +0530

    include backend

---
+++ backend/config/auth/userPresent.js
@@ -0,0 +1,13 @@
+const userPresent = (request, response, done) => {
+  if (request.cookies.userToken) {
+    request.headers = {
+      ...request.headers,
+      authorization: request.cookies.userToken,
+    };
+
+    done(null, true);
+  } else {
+    done(null, false);
+  }
+};
+module.exports = userPresent;
---
+++ backend/routes/api/users.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+
+const userPresent = require("../../config/auth/userPresent");
+
+const {
+  createNewUser,
+  userLogin,
+  sendUserJwt,
+  getUserDetails,
+  updateUserProfile,
+  destroySession,
+} = require("../../controllers/users_api");
+
+router.get(
+  "/destroy-session",
+  passport.authenticate("jwt", { session: false }),
+  destroySession
+);
+
+router.post(
+  "/update-user-profile",
+  passport.authenticate("jwt", { session: false }),
+  updateUserProfile
+);
+router.get(
+  "/isAuthorized",
+  userPresent,
+  passport.authenticate("jwt", { session: false }),
+  sendUserJwt
+);
+router.get(
+  "/get-user-data",
+  passport.authenticate("jwt", { session: false }),
+  getUserDetails
+);
+router.post("/create-new-user", createNewUser);
+router.post("/login", userLogin);
+
+module.exports = router;
